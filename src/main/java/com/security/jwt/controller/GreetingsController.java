bd98e7f866a24d4cce40eeea47cab847faab960c
Alimul Razi <alimul.razi@buddi.co.uk>
2025-07-29 11:55:56 +0100

    Initial commit: spring security with using an in memory database.

---
+++ src/main/java/com/security/jwt/controller/GreetingsController.java
@@ -0,0 +1,28 @@
+package com.security.jwt.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin";
+    }
+
+
+}
